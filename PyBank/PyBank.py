10d04426bf1b133bcdf78f47bbb197bfedc2b78b
RSPXRSP <39788183+RSPXRSP@users.noreply.github.com>
2019-03-11 23:11:50 -0600

    Learning to use GitHub push.

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,50 +1,40 @@
 import os
 import csv
+import pandas as pd
 
-conteo = 0
-tprofit = 0
-valores,fechas = [],[]
 path = '/Users/rspxrsp/Documents/GitLab/TECMC201902DATA2/02-Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv'
+open_path = open(path, mode="r")
+reader = csv.reader(open_path, delimiter=',')
+row_count = len(list(reader)) - 1
 
-with open(path, mode="r") as archivo:
-    cabecera = next(archivo)
-    leer = csv.reader(archivo, delimiter=',')
-    conteo = sum(1 for row in leer)
+open_path.seek(0)
+for column in reader:
+    pass
 
-with open(path, mode="r") as archivo:
-    leer = csv.reader(archivo, delimiter=',')
-    cabecera = next(archivo)
-    for row in leer:
-        tprofit += int(row[1])
+df = pd.read_csv(path)
+total = df.iloc[:,1].sum()
+total = int(total)
 
-cambio = []
-neto = []
-with open(path, mode="r") as archivo:
-    leer = csv.reader(archivo, delimiter=',')
-    cabecera = next(archivo)
-    for row in leer:
-        valores.append(int(row[1]))
-        fechas.append(row[0])
-    antes = int(valores[0])
-    for cambio in valores:
-        listanueva =  int(cambio) - int(antes)
-        antes = cambio
-        neto = neto + [listanueva]
+df['Change'] = df.iloc[:,1].diff()
+average = df.iloc[:,2].mean()
+average = int(average)
 
-conteo2 = 0
-conteo2 = (sum(1 for elemento in neto))-1
-suma = 0
-for elementos in neto:
-    suma = elementos + suma
-promedio = suma / conteo2
+max_loss = df.iloc[:,2].min()
+max_loss = int(max_loss)
 
-valmax = max(neto)
-valmin = min(neto)
-bvalmax = neto.index(valmax)
-bvalmin = neto.index(valmin)
-fechamax = fechas[bvalmax]
-fechamin = fechas[bvalmin]
+df.columns = ['Date', 'Earning', 'Change']
 
-escribir = open("Analisis.txt","a")
-escribir.write('Financial Analysis\n----------------------------\nTotal Months: '+str(conteo)+'\nTotal: '+str(tprofit)+'\nAverage  Change: '+str(promedio)+'\nGreatest Increase in Profits: '+str(fechamax)+' ('+str(valmax)+')'+'\nGreatest Decrease in Profits: '+str(fechamin)+' ('+str(valmin)+')')
-escribir.close()
+date_loss = df.loc[df['Change'] == (max_loss)].index
+date_loss = df.iloc[date_loss,0]
+date_loss = date_loss.real[0]
+
+max_profit = df.iloc[:,2].max()
+max_profit = int(max_profit)
+
+date_profit = df.loc[df['Change'] == (max_profit)].index
+date_profit = df.iloc[date_profit,0]
+date_profit = date_profit.real[0]
+
+new_file = open("Analisis.txt","a")
+new_file.write('Financial Analysis\n----------------------------------------------------\nTotal Months: '+str(row_count)+'\nTotal: $ '+str(total)+'\nAverage Change: $ '+str(average)+'\nGreatest Increase in Profits: '+str(date_profit)+' ($ '+str(max_profit)+')'+'\nGreatest Decrease in Profits: '+str(date_loss)+' ($ '+str(max_loss)+')')
+new_file.close()
